bf95e7e1edae5a8ab97241c534560e31b9d34c7d
ikram-cyber <ikram.ujrc@gmail.com>
2026-04-23 22:01:44 +0700

    Security Update: Injected actual Firebase Matrix Keys

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCGd_L4Es00xZLpL8HDHgm11kPfWUvqf3U",
-  authDomain: "converter-48111.firebaseapp.com",
-  projectId: "converter-48111",
-  storageBucket: "converter-48111.firebasestorage.app",
-  messagingSenderId: "352175968375",
-  appId: "1:352175968375:web:7c72b11f7ab120de20f672"
+// KUNCI RAHASIA MATRIX ZYNC
+export const firebaseConfig = {
+  apiKey: "AIzaSyDrNPFvNC-qqB2slfY0F6rtNd1L1cQHAcI",
+  authDomain: "ikram-social.firebaseapp.com",
+  projectId: "ikram-social",
+  storageBucket: "ikram-social.firebasestorage.app",
+  messagingSenderId: "250435295406",
+  appId: "1:250435295406:web:97dd4e2f5ea8842be7353f"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
